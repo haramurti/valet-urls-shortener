7aca9181a7e3095450e4df1d36ded3590cdbe052
nevalhr <phneval@gmail.com>
2026-04-26 12:12:03 +0700

    Feat :adding config file

--- config/config.go
+++ config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	databaseURL string
+}
+
+func Load() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("env not found")
+	}
+
+	return &Config{
+		databaseURL: mustGetEnv("DATABASE_URL"),
+	}
+
+}
+
+func mustGetEnv(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatal("env value is missing")
+		log.Fatalf(" ENV '%s' need to be filled", key)
+	}
+	return value
+}
