a388e9b448c3d0ac179ee2e087828c82c5a53318
nevalhr <phneval@gmail.com>
2026-04-26 13:12:37 +0700

    feat : setting up bootstrap

---
+++ cmd/bootstrap/bootstrap.go
@@ -0,0 +1,38 @@
+package bootstrap
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/haramurti/valet-urls-shortener/Internal/infra/database"
+	"github.com/haramurti/valet-urls-shortener/config"
+	"gorm.io/gorm"
+)
+
+type App struct {
+	Fiber  *fiber.App
+	DB     *gorm.DB
+	Config *config.Config
+}
+
+func NewApp() *App {
+	cfg := config.Load()
+
+	db, err := database.Connect(cfg.DatabaseURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to database")
+		panic(err)
+	}
+
+	//Migration
+	err = database.AutoMigrate(db)
+	if err != nil {
+		log.Fatalf("Failed to migrate database")
+		panic(err)
+	}
+	return &App{
+		Fiber:  fiber.New(),
+		DB:     db,
+		Config: cfg,
+	}
+}
